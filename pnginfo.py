44e4170c24a4232cabca2257cb6e741428eadaf7
username <username@example.com>
2022-10-23 16:15:08 +0900

    Updated

--- pnginfo.py
+++ pnginfo.py
@@ -30,56 +30,58 @@ img = Image.open(sys.argv[1])
 
 # NovelAI
 if "Software" in img.text:
-    # print("Software :" + img.text["Software"])
+    if img.text["Software"] == "NovelAI":
+        # print("Software :" + img.text["Software"])
 
-    # if "Source" in img.text:
-    #     print("Source :" + img.text["Source"])
+        # if "Source" in img.text:
+        #     print("Source :" + img.text["Source"])
 
-    if "Description" in img.text:
-        Description = img.text["Description"].replace("{","(")
-        Description = Description.replace("}",")")
-        Description = Description.replace("\"","")
+        if "Description" in img.text:
+            Description = img.text["Description"].replace("{","(")
+            Description = Description.replace("}",")")
+            Description = Description.replace("\"","")
 
-        print(Description)
+            print(Description)
 
-    if "Comment" in img.text:
-        Comment_dict = json.loads(img.text["Comment"])
-        if "uc" in Comment_dict:
+        if "Comment" in img.text:
+            Comment_dict = json.loads(img.text["Comment"])
+            if "uc" in Comment_dict:
 
-            Negative_prompt = Comment_dict["uc"].replace("{","(")
-            Negative_prompt = Negative_prompt.replace("}",")")
-            Negative_prompt = Negative_prompt.replace("\"","")
+                Negative_prompt = Comment_dict["uc"].replace("{","(")
+                Negative_prompt = Negative_prompt.replace("}",")")
+                Negative_prompt = Negative_prompt.replace("\"","")
 
-            print("Negative prompt: " + Negative_prompt)
+                print("Negative prompt: " + Negative_prompt)
 
-            if Comment_dict["sampler"] == "k_euler_ancestral":
-                Sampler = "Euler a"
-            elif Comment_dict["sampler"] == "k_euler":
-                Sampler = "Euler"
-            elif Comment_dict["sampler"] == "k_lms":
-                Sampler = "LMS"
-            elif Comment_dict["sampler"] == "plms":
-                Sampler = "PLMS"
-            elif Comment_dict["sampler"] == "ddim":
-                Sampler = "DDIM"
-            else:
-                Sampler = "Euler a"
+                if Comment_dict["sampler"] == "k_euler_ancestral":
+                    Sampler = "Euler a"
+                elif Comment_dict["sampler"] == "k_euler":
+                    Sampler = "Euler"
+                elif Comment_dict["sampler"] == "k_lms":
+                    Sampler = "LMS"
+                elif Comment_dict["sampler"] == "plms":
+                    Sampler = "PLMS"
+                elif Comment_dict["sampler"] == "ddim":
+                    Sampler = "DDIM"
+                else:
+                    Sampler = "Euler a"
 
-            w, h = img.size
-            img_size = str(w) + "x" + str(h)
+                w, h = img.size
+                img_size = str(w) + "x" + str(h)
 
-            print("Steps: " + str(Comment_dict["steps"]) + ", " +
-                  "Sampler: " + Sampler + ", " +
-                  "CFG scale: " + str(Comment_dict["scale"]) + ", " +
-                  "Seed: " + str(Comment_dict["seed"]) + ", " +
-                  "Size: " + img_size + ", " +
-                # "Noise: 不明" + Comment_dict["noise"] ","+
-                  "Denoising strength: " + str(Comment_dict["strength"]) + ", " +
-                  "Clip skip: 2, " +
-                  "ENSD: 31337"
-                  )
-
-        # print("Source" + img.text["Comment"])
+                print("Steps: " + str(Comment_dict["steps"]) + ", " +
+                    "Sampler: " + Sampler + ", " +
+                    "CFG scale: " + str(Comment_dict["scale"]) + ", " +
+                    "Seed: " + str(Comment_dict["seed"]) + ", " +
+                    "Size: " + img_size + ", " +
+                    # "Noise: 不明" + Comment_dict["noise"] ","+
+                    "Denoising strength: " + str(Comment_dict["strength"]) + ", " +
+                    "Clip skip: 2, " +
+                    "ENSD: 31337"
+                    )
+            # print("Source" + img.text["Comment"])
+    else:
+        print("Nothing found in the image.")
 
 # Stable Diffusion Web UI
 elif "parameters" in img.text:
